69f767666bc4bb58b878291090556919e3b75164
Marta de Miguel <m.demiguelr@alumnos.upm.es>
2018-05-22 22:55:32 +0200

    Step 10- Heroku Postgres1

--- app.js
+++ app.js
@@ -18,27 +18,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
-// In production (Heroku) I redirect the HTTP requests to https.
-// Documentation: http://jaketrent.com/post/https-redirect-node-heroku/
-if (app.get('env') === 'production') {
-    app.use(function(req, res, next) {
-        if (req.headers['x-forwarded-proto'] !== 'https') {
-            res.redirect('https://' + req.get('Host') + req.url);
-        } else {
-            next()
-        }
-    });
-}
-
-
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(methodOverride('_method',{methods: ["POST","GET"]}));
 // Configuracion de la session para almacenarla en BBDD usando Sequelize.
 var sequelize = require("./models");
 var sessionStore = new SequelizeStore({
@@ -59,36 +45,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(partials());
 app.use(flash());
 
-// Dynamic Helper:
-app.use(function(req, res, next) {
-
-    // To use req.session in the views
-    res.locals.session = req.session;
-
-    // To use req.url in the views
-    res.locals.url = req.url;
-    
-    next();
-});
-
 app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
